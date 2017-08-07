d04ff5117fe3458a84f27800c0587ff98a9a220b
Nelluri Srikanth (Product Engineering Service) <NE336724@wipro.com>
2017-08-07 17:27:58 +0530

    fixed new checkstyle warns

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  * @param someone
-  * @return greeting
+  *@param someone
+  *@return greeting
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,5 @@
 /**
- * package name
+ * package name.
  */
 
 package com.example;
